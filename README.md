73018a58a5d4ee0a5873e0b01e288311dd2a5814
Eric Lagergren <ericscottlagergren@gmail.com>
2016-01-27 13:22:52 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # nil
 A small package for nillable types.
+
+Package nilable is an easy way to easily use Go types while still
+distinguishing between an absent value and a zero-type.
+
+This distinction (absent versus zero) can occur in APIs that unmarshal
+a JSON payload into a specific type. If the payload does not set specific
+JSON fields, the Go type it's marshaled into will set the unspecified fields
+to their zero values.
+
+This is remedied in one of two ways:
+
+First, the API could use "replace" semantics instead of "edit." This means
+the object or data sent through the API is replacing what it represents on
+the server. In practical terms, this could be represented by first getting
+the entire object from the server, modifying the necessary fields, and then
+sending the entire object back.
+
+While this works, it requires two round-trips to the server and could allow
+for race conditions, depending how the API is structured.
+
+The second remedy is to use something similar to this package: nilable values.
+Nilable values would solve the previous problem by creating a distinction
+between absent and zero. This means instead of two round-trips to the server,
+the client would only have to send one request containing the modified fields.
+
+All basic types in package nilable implement the Nilable interface.
+
+The basic structure of a nilable type is a struct containing a pointer to the
+native type.
+
+For example:
+
+		type String struct {
+			s *string
+		}
+
+Each type has a method which returns the underlying type. By convention,
+this method is the name of the Nilable type, which is also the exported
+version of the name of the underlying type.
+
+		int64 -> Int64
+		string -> String
+		bool -> Bool
+
+Each type has a String method which returns the canonical string version
+of the type. (As it would normally be printed using the "%s" verb.)
+One caveat is nil -- if the type is nil, "<nil>" will be returned.
+
+Each type should be passed by value like a normal Go type. (I.e., do not
+pass around *T.)
+
+# Documentation
+
+[Godoc](https://godoc.org/github.com/SermoDigital/nilable)
---
+++ nil.go
@@ -0,0 +1,237 @@
+package nilable
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"strconv"
+	"time"
+)
+
+type Nilable interface {
+	IsNil() bool
+}
+
+type String struct {
+	s *string
+}
+
+func NewString(s string) String {
+	return String{s: &s}
+}
+
+func (n String) IsNil() bool {
+	return n.s == nil
+}
+
+func (n String) String() string {
+	if n.IsNil() {
+		return ""
+	}
+	return *n.s
+}
+
+func (n *String) Scan(value interface{}) error {
+	if value == nil {
+		return errors.New("nilable.String.Scan: value is nil")
+	}
+	switch v := value.(type) {
+	case string:
+		n.s = &v
+	case []byte:
+		vb := string(v)
+		n.s = &vb
+	default:
+		return errors.New("nilable.String.Scan: value is an incorrect type")
+	}
+	return nil
+}
+
+func (n String) Value() (driver.Value, error) {
+	return n.String(), nil
+}
+
+func (n String) MarshalJSON() ([]byte, error) {
+	return json.Marshal(n.String())
+}
+
+func (n *String) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &n.s)
+}
+
+type Bool struct {
+	b *bool
+}
+
+func NewBool(b bool) Bool {
+	return Bool{b: &b}
+}
+
+func (n Bool) IsNil() bool {
+	return n.b == nil
+}
+
+func (n Bool) Bool() bool {
+	if n.IsNil() {
+		return false
+	}
+	return *n.b
+}
+
+func (n *Bool) Scan(value interface{}) error {
+	if value == nil {
+		return errors.New("nilable.Bool.Scan: value is nil")
+	}
+	switch v := value.(type) {
+	case bool:
+		n.b = &v
+	default:
+		return errors.New("nilable.Bool.Scan: value is an incorrect type")
+	}
+	return nil
+}
+
+func (n Bool) Value() (driver.Value, error) {
+	return n.Bool(), nil
+}
+
+func (n Bool) MarshalJSON() ([]byte, error) {
+	return json.Marshal(n.Bool())
+}
+
+func (n *Bool) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &n.b)
+}
+
+func (n Bool) String() string {
+	switch {
+	case n.IsNil():
+		return "<nil>"
+	case n.Bool():
+		return "true"
+	default:
+		return "false"
+	}
+}
+
+type Time struct {
+	t *time.Time
+}
+
+func NewTime(t time.Time) Time {
+	return Time{t: &t}
+}
+
+func (n Time) IsNil() bool {
+	return n.t == nil
+}
+
+func (n Time) Time() time.Time {
+	if n.IsNil() {
+		return time.Time{}
+	}
+	return time.Time(*n.t)
+}
+
+func (n *Time) Scan(value interface{}) error {
+	if value == nil {
+		return errors.New("nilable.Time.Scan: value is nil")
+	}
+	switch v := value.(type) {
+	case time.Time:
+		n.t = &v
+	default:
+		return errors.New("nilable.Time.Scan: value is an incorrect type")
+	}
+	return nil
+}
+
+func (n Time) String() string {
+	if n.IsNil() {
+		return "<nil>"
+	}
+	return n.t.String()
+}
+
+func (n Time) Value() (driver.Value, error) {
+	return n.Time(), nil
+}
+
+func (n Time) MarshalJSON() ([]byte, error) {
+	return json.Marshal(n.Time())
+}
+
+func (n *Time) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &n.t)
+}
+
+type Int64 struct {
+	n *int64
+}
+
+func NewInt64(n int64) Int64 {
+	return Int64{n: &n}
+}
+
+func (n Int64) IsNil() bool {
+	return n.n == nil
+}
+
+func (n Int64) Int64() int64 {
+	if n.IsNil() {
+		return 0
+	}
+	return *n.n
+}
+
+func (n *Int64) Scan(value interface{}) error {
+	if value == nil {
+		return errors.New("nilable.Int64.Scan: value is nil")
+	}
+	switch v := value.(type) {
+	case int64:
+		n.n = &v
+	case int:
+		nv := int64(v)
+		n.n = &nv
+	case int32:
+		nv := int64(v)
+		n.n = &nv
+	case int16:
+		nv := int64(v)
+		n.n = &nv
+	case int8:
+		nv := int64(v)
+		n.n = &nv
+	default:
+		return errors.New("nilable.Int64.Scan: value is an incorrect type")
+	}
+	return nil
+}
+
+func (n Int64) Value() (driver.Value, error) {
+	return n.Int64(), nil
+}
+
+func (n Int64) MarshalJSON() ([]byte, error) {
+	return json.Marshal(n.Int64())
+}
+
+func (n *Int64) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &n.n)
+}
+
+func (n Int64) String() string {
+	if n.IsNil() {
+		return "<nil>"
+	}
+	return strconv.FormatInt(*n.n, 10)
+}
+
+var (
+	_ Nilable = (*String)(nil)
+	_ Nilable = (*Bool)(nil)
+	_ Nilable = (*Bool)(nil)
+	_ Nilable = (*Int64)(nil)
+	_ Nilable = (*Time)(nil)
+)
